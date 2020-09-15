5b4c6671fc58938f54bb0b9d5d31f873548f9809
i-loading <69751600+i-loading@users.noreply.github.com>
2020-09-15 08:06:29 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
-   $('.burger').click(function () { 
+   $('.burger').click(function (e) { 
+      e.preventDefault();
       $('.menu_burger').toggleClass("menu_burger-active");
+   });
+   $('.input_wrapper').click(function (e) { 
+      e.preventDefault();
+      $('.input_wrapper span').toggleClass('active');
+   });
+   $('.input_wrapper').focusout(function (e) { 
+      e.preventDefault();
+      $('.input_wrapper span').toggleClass('active');
    });
 });
